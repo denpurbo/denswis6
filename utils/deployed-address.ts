3b0bd017afeb12dd81e171fa390389281d99865b
den purbo <91820256+denpurbo@users.noreply.github.com>
2024-07-30 11:22:34 +0700

    Update deployed-address.ts

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0xCaB3F401c57ff02233aa862cCD8c53661ceFe432'
+export const ProxyAdmin = '0x697D98EA90AbdffA7bE21ce7d8056202B9b27235'
+export const SWTRImplementation = '0x39Bfb727768a3c15631b91c60C4F7e696e9E3665'
     
